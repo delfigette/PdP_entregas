3fe12125daeb3906bbcf8aef482733e47e5dfe99
delfigette <49289431+delfigette@users.noreply.github.com>
2020-05-15 21:36:48 -0300

    Segunda entrega con correcciones

--- trabajoPractico_3.hs
+++ trabajoPractico_3.hs
@@ -1,19 +1,24 @@
 import Text.Show.Functions()
 import Data.List (genericLength)
 
+type Propiedad = (String, Int)
+
+
 data Persona = Persona   {
                                 nombrePersona :: String, 
                                 tacticaDeJuego :: String, 
                                 cantidadDeDinero :: Int, 
-                                propiedadesCompradas :: [(String, Int)], 
-                                acciones :: [String]
-                            } deriving (Show, Eq)
-                            
+                                propiedadesCompradas :: [Propiedad], 
+                                acciones :: [Accion]
+                            } deriving (Show)
+
+type Accion = Persona -> Persona
+
 -- cargar Jugadores
 
-carolina = Persona "Carolina" "Accionista" 0 [] ["pagarAAccionista"]
+carolina = Persona "Carolina" "Accionista" 500 [] [pagarAAccionistas, pasarPorElBanco]
 
-manuel = Persona "Manuel" "Oferente singular" 0 [] ["enoajrse"]
+manuel = Persona "Manuel" "Oferente singular" 500 [] [enojarse, pasarPorElBanco]
 
 --Alias útiles por si en un futuro decide cambiarse la cantidad de dinero que se da
 dineroComienzoJuego :: Int
@@ -35,19 +40,14 @@ cambiarTacticaDeJuego unaFuncion unaPersona = unaPersona {tacticaDeJuego = unaFu
 
 -- OTRAS FUNCIONES
 
-agregarAcciones :: String -> Persona-> Persona
+agregarAcciones :: Accion -> Persona -> Persona
 agregarAcciones accion unaPersona = unaPersona {acciones = accion : acciones unaPersona}
 
-agregarPropiedades :: (String, Int) -> Persona ->  Persona
+agregarPropiedades :: Propiedad -> Persona ->  Persona
 agregarPropiedades unaPropiedad unaPersona = unaPersona {propiedadesCompradas = unaPropiedad : propiedadesCompradas unaPersona}
 
 tieneLaTactica :: String -> Persona -> Bool
 tieneLaTactica tactica unaPersona = tacticaDeJuego unaPersona == tactica
-
---Accion agregada para comenzar juego
-
-comenzarJuego :: Persona -> Persona
-comenzarJuego unaPersona = (cambiarCantidadDeDinero ( + dineroComienzoJuego) . (agregarAcciones "pasarPorElBanco")) unaPersona
 
 
 -- Acciones
@@ -56,7 +56,7 @@ pasarPorElBanco :: Persona -> Persona
 pasarPorElBanco unaPersona = (cambiarTacticaDeJuego (const "Comprador compulsivo") . cambiarCantidadDeDinero ( + dineroPorPasarPorBanco)) unaPersona
 
 enojarse :: Persona -> Persona
-enojarse unaPersona = (cambiarCantidadDeDinero (+50) . agregarAcciones ("gritar")) unaPersona
+enojarse unaPersona = (cambiarCantidadDeDinero (+50) . agregarAcciones (gritar)) unaPersona
 
 gritar :: Persona -> Persona
 gritar unaPersona = cambiarNombrePersona ("AHHHH"++) unaPersona
@@ -66,17 +66,17 @@ puedeSubastar "Oferente singular" = True
 puedeSubastar "Accionista" = True
 puedeSubastar _ = False
 
-subastar :: Persona -> (String, Int) -> Persona
+subastar :: Persona -> Propiedad -> Persona
 subastar unaPersona (nombrePropiedad, precio)
-    | puedeSubastar (tacticaDeJuego unaPersona) = (cambiarCantidadDeDinero (precio-) . agregarPropiedades ((nombrePropiedad, precio))) unaPersona
+    | puedeSubastar (tacticaDeJuego unaPersona) = (cambiarCantidadDeDinero (subtract precio) . agregarPropiedades ((nombrePropiedad, precio))) unaPersona
     | otherwise = unaPersona
 
 --cobrarAlquileres
 
-esBarata :: (String, Int) -> Bool
+esBarata :: Propiedad -> Bool
 esBarata (_, precio) = precio < 150
 
-dineroACobrarPorAlquileres :: [(String, Int)] -> Int
+dineroACobrarPorAlquileres :: [Propiedad] -> Int
 dineroACobrarPorAlquileres propiedades = length (filter esBarata propiedades) * 10 + length (filter (not.esBarata) propiedades) * 20 
 
 cobrarAlquileres :: Persona -> Persona
@@ -87,4 +87,29 @@ cobrarAlquileres unaPersona = cambiarCantidadDeDinero ( + (dineroACobrarPorAlqui
 pagarAAccionistas :: Persona -> Persona
 pagarAAccionistas unaPersona 
                                 | tacticaDeJuego unaPersona == "Accionista" = cambiarCantidadDeDinero ( + 200) unaPersona
-                                | otherwise = cambiarCantidadDeDinero (100 - ) unaPersona
+                                | otherwise = cambiarCantidadDeDinero (subtract 100) unaPersona
+
+--hacerBerrinchePor
+
+hacerBerrinchePor :: Persona -> Propiedad -> Persona
+hacerBerrinchePor unaPersona unaPropiedad = (agregarPropiedades (unaPropiedad) . gritar . cambiarCantidadDeDinero (+10)) unaPersona 
+
+--ultimaRonda 
+ultimaRonda :: Persona -> Persona
+ultimaRonda unaPersona
+  = foldl (\persona accion -> accion persona) unaPersona (acciones unaPersona)
+
+-- Me había propuesto hacerlo utilizando max, pero me fue difícil hacer la comparación entre dos personas sólo  por su cantidad de dinero
+-- la idea incluso era después hacer contarDinero como una funcion lambda
+-- pero no me funcionaba, así que preferí hacerlo de otra forma
+
+-- juegoFinal :: Persona -> Persona -> Persona
+-- juegoFinal persona1 persona2 = max (contarDinero (persona1)) (contarDinero (persona2))
+
+-- contarDinero :: Persona -> Int
+-- contarDinero unaPersona = cantidadDeDinero (ultimaRonda unaPersona)
+
+juegoFinal :: Persona -> Persona -> Persona
+juegoFinal unaPersona otraPersona
+  | cantidadDeDinero (ultimaRonda unaPersona) < cantidadDeDinero (ultimaRonda otraPersona) = otraPersona
+  | otherwise = otraPersona
